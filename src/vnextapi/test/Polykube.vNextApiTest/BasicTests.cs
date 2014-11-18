f146e8820dd61363eb24d143499c5b39f98d8c73
Cole Mickens <cole.mickens@gmail.com>
2014-11-18 01:56:05 +0000

    Add xunit tests, add VS2015 files for others

---
+++ src/vnextapi/test/Polykube.vNextApiTest/BasicTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+// https://github.com/aspnet/Testing/wiki/How-to-create-test-projects
+
+namespace Polykube.vNextApiTest
+{   
+    public class BasicTests
+    {
+        [Fact]
+        public void BasicTest01()
+        {
+            Assert.True(true);
+        }
+    }
+}
