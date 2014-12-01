f4e3fb55252d96db958173f4986129498f7eef45
Cole Mickens <cole.mickens@gmail.com>
2014-12-01 05:18:01 +0000

    Remove nowin, add passthrough controller

---
+++ services/vnextapi/src/Polykube.vNextApi/Controllers/PassthroughController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Polykube.vNextApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PassthroughController : Controller
+    {
+        [HttpGet]
+        public async Task<string> Index()
+        {
+            var client = new HttpClient();
+            var resp = await client.GetAsync("http://goapi.default.kubernetes.local/");
+            var content = await resp.Content.ReadAsStringAsync();
+            return content;
+        }
+    }
+}
