41e900758534639a70d3ef832f2297bcc545339b
Cole Mickens <cole.mickens@gmail.com>
2015-01-29 15:05:44 -0800

    Add Bond example and test

---
+++ services/vnextapi/src/Polykube.Common/Model/BaseThing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Bond;
+using Bond.Protocols;
+using Bond.IO.Safe;
+
+namespace Polykube.Common.Model
+{
+    [Bond.Schema]
+    public class BaseThing
+    {
+        [Bond.Id(0)]
+        public string Name { get; set; }
+    }
+}
